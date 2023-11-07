ffab98c5c9b51fff09ee42219f3ef9259cbd9508
15achi <achikopapiashvili15@gmail.com>
2023-11-07 15:02:30 +0400

    testy

--- NPOIExample/Models/Student.cs
+++ NPOIExample/Models/Student.cs
@@ -12,6 +12,6 @@ namespace NPOIExample.Models
         public string Name { get; set; }
         public string Roll { get; set; }
 
-        public string Address { get; set; }
+        public string Address { get; set;  }
     }
 }
